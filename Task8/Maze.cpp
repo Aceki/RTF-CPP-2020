17bfcf34a3a3bea54ccad8f1cc5b6bf4303e901c
Aceki <aceki320@gmail.com>
2020-12-08 13:14:25 +0500

    ref: Task8

--- Task8/Maze.cpp
+++ Task8/Maze.cpp
@@ -3,91 +3,6 @@
 
 #include "Maze.h"
 
-Maze::side Maze::getNeighbourSide(int i1, int j1, int i2, int j2) const
-{
-	if (!inBounds(i1, j1) || !inBounds(i2, j2))
-		return s_none;
-
-	int offset_i = i2 - i1;
-	int offset_j = j2 - j1;
-
-	if (offset_i == -1)
-		return s_up;
-	if (offset_i == 1)
-		return s_down;
-	if (offset_j == -1)
-		return s_left;
-	if (offset_j == 1)
-		return s_right;
-
-	return s_none;
-}
-
-bool Maze::inBounds(int i, int j) const
-{
-	return i >= 0 && j >= 0 && ((j * m_n + i) < (m_n * m_m));
-}
-
-wchar_t Maze::getPathChar(path path)
-{
-	switch (path)
-	{
-	case p_none:
-		return L'0';
-	case p_up:
-		return L'0';
-	case p_down:
-		return L'0';
-	case p_left:
-		return L'0';
-	case p_right:
-		return L'0';
-	case p_up_down:
-		return L'|';
-	case p_up_down_left:
-		return L'┤';
-	case p_down_left:
-		return L'┐';
-	case p_up_right:
-		return L'└';
-	case p_up_left_right:
-		return L'┴';
-	case p_down_left_right:
-		return L'┬';
-	case p_up_down_right:
-		return L'├';
-	case p_left_right:
-		return L'─';
-	case p_up_down_left_right:
-		return L'┼';
-	case p_up_left:
-		return L'┘';
-	case p_down_right:
-		return L'┌';
-	default:
-		return L'E';
-	}
-}
-
-void Maze::toggleConnection(int i, int j, side side)
-{
-	switch (side)
-	{
-	case s_up:
-		cell(i - 1, j).m_down = !cell(i - 1, j).m_down;
-		break;
-	case s_right:
-		cell(i, j).m_right = !cell(i, j).m_right;
-		break;
-	case s_down:
-		cell(i, j).m_down = !cell(i, j).m_down;
-		break;
-	case s_left:
-		cell(i, j - 1).m_right = !cell(i, j - 1).m_right;
-		break;
-	}
-}
-
 Maze::Maze(int n, int m) : m_n(n), m_m(m)
 {
 	m_field = new MCell[n * m];
@@ -95,75 +10,189 @@ Maze::Maze(int n, int m) : m_n(n), m_m(m)
 
 MCell& Maze::cell(int i, int j) const
 {
-	if (!inBounds(i, j))
-		throw "Index out of range!";
 	return m_field[j * m_n + i];
 }
 
 void Maze::printMaze() const
 {
+	enum path
+	{
+		p_none = 0b0000,
+		p_up = 0b1000,
+		p_right = 0b0100,
+		p_down = 0b0010,
+		p_left = 0b0001,
+		p_up_down = p_up | p_down,
+		p_up_down_left = p_up | p_down | p_left,
+		p_down_left = p_down | p_left,
+		p_up_right = p_up | p_right,
+		p_up_left_right = p_up | p_left | p_right,
+		p_down_left_right = p_down | p_left | p_right,
+		p_up_down_right = p_up | p_down | p_right,
+		p_left_right = p_left | p_right,
+		p_up_down_left_right = p_up | p_down | p_left | p_right,
+		p_up_left = p_up | p_left,
+		p_down_right = p_down | p_right,
+	};
+
 	for (int m = 0; m < m_m; m++)
 	{
 		for (int n = 0; n < m_n; n++)
 		{
-			bool up = inBounds(m - 1, n) && cell(m - 1, n).m_down;
+			bool up = cell(m - 1, n).m_down && m > 0;
 			bool down = cell(m, n).m_down;
-			bool left = inBounds(m, n - 1) && cell(m, n - 1).m_right;
+			bool left = cell(m, n - 1).m_right && n > 0;
 			bool right = cell(m, n).m_right;
 
-			int path = path::p_none;
-			path |= (up * p_up) | (down * p_down) | (left * p_left) | (right * p_right);
+			path p = static_cast<path>((p_none) | (up * p_up) | (down * p_down) | (left * p_left) | (right * p_right));
 
-			std::wcout << Maze::getPathChar(static_cast<Maze::path>(path));
+			wchar_t path_char;
+
+			switch (p)
+			{
+			case p_none:
+				path_char = L'0';
+				break;
+			case p_up:
+				path_char = L'0';
+				break;
+			case p_down:
+				path_char = L'0';
+				break;
+			case p_left:
+				path_char = L'0';
+				break;
+			case p_right:
+				path_char = L'0';
+				break;
+			case p_up_down:
+				path_char = L'|';
+				break;
+			case p_up_down_left:
+				path_char = L'┤';
+				break;
+			case p_down_left:
+				path_char = L'┐';
+				break;
+			case p_up_right:
+				path_char = L'└';
+				break;
+			case p_up_left_right:
+				path_char = L'┴';
+				break;
+			case p_down_left_right:
+				path_char = L'┬';
+				break;
+			case p_up_down_right:
+				path_char = L'├';
+				break;
+			case p_left_right:
+				path_char = L'─';
+				break;
+			case p_up_down_left_right:
+				path_char = L'┼';
+				break;
+			case p_up_left:
+				path_char = L'┘';
+				break;
+			case p_down_right:
+				path_char = L'┌';
+				break;
+			default:
+				path_char = L'E';
+				break;
+			}
+
+			std::wcout << path_char;
 		}
 		std::cout << std::endl;
-	}
+	}	
 }
 
 bool Maze::hasConnection(int i1, int j1, int i2, int j2) const
 {
-	switch (getNeighbourSide(i1, j1, i2, j2))
-	{
-	case s_up:
+	int offset_i = i2 - i1;
+	int offset_j = j2 - j1;
+
+	if (offset_i == -1)
 		return cell(i2, j2).m_down;
-	case s_down:
+	if (offset_i == 1)
 		return cell(i1, j1).m_down;
-	case s_left:
+	if (offset_j == -1)
 		return cell(i2, j2).m_right;
-	case s_right:
+	if (offset_j == 1)
 		return cell(i1, j1).m_right;
-	default:
-		return false;
-	}
+
+	return false;
 }
 
 bool Maze::makeConnection(int i1, int j1, int i2, int j2)
 {
 	if (!hasConnection(i1, j1, i2, j2))
-		toggleConnection(i1, j1, getNeighbourSide(i1, j1, i2, j2));
+	{
+		int offset_i = i2 - i1;
+		int offset_j = j2 - j1;
+
+		if (offset_i == -1)
+		{
+			cell(i2, j2).m_down = true;
+			return true;
+		}
+		if (offset_i == 1)
+		{
+			cell(i1, j1).m_down = true;
+			return true;
+		}
+		if (offset_j == -1)
+		{
+			cell(i2, j2).m_right = true;
+			return true;
+		}
+		if (offset_j == 1)
+		{
+			cell(i1, j1).m_right = true;
+			return true;
+		}
+
+		return false;
+	}
 	return true;
 }
 
 bool Maze::removeConnection(int i1, int j1, int i2, int j2)
 {
 	if (hasConnection(i1, j1, i2, j2))
-		toggleConnection(i1, j1, getNeighbourSide(i1, j1, i2, j2));
+	{
+		int offset_i = i2 - i1;
+		int offset_j = j2 - j1;
+
+		if (offset_i == -1)
+		{
+			cell(i2, j2).m_down = false;
+			return true;
+		}
+		if (offset_i == 1)
+		{
+			cell(i1, j1).m_down = false;
+			return true;
+		}
+		if (offset_j == -1)
+		{
+			cell(i2, j2).m_right = false;
+			return true;
+		}
+		if (offset_j == 1)
+		{
+			cell(i1, j1).m_right = false;
+			return true;
+		}
+
+		return false;
+	}
 	return true;
 }
 
 Maze::~Maze()
 {
 	delete[] m_field;
-}
-
-void fillMaze(Maze& maze, const MTreeNode* startNode)
-{
-	if (startNode->childCount() == 0)
-		return;
-	for (int i = 0; i < startNode->childCount(); i++)
-	{
-		const MTreeNode* child = startNode->child(i);
-		maze.makeConnection(startNode->i(), startNode->j(), child->i(), child->j());
-		fillMaze(maze, child);
-	}
 }
--- Task8/main.cpp
+++ Task8/main.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
+﻿#include <iostream>
 
 #include "Maze.h"
-#include "MTreeNode.h"
 
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "en_US.UTF-8");
+    
+    Maze maze(5, 5);
 
-    constexpr int mazeRows = 5;
-    constexpr int mazeColumns = 5;
-
-    Maze maze1(mazeRows, mazeColumns);
-    for (int i = 0; i <= 4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        maze1.makeConnection(i - 1, i, i, i);
-        maze1.makeConnection(i, i, i, i + 1);
+        maze.makeConnection(i - 1, i, i, i);
+        maze.makeConnection(i, i, i, i + 1);
     }
-    maze1.printMaze();
+    maze.makeConnection(3, 4, 4, 4);
 
-    Maze maze2(mazeRows, mazeColumns);
-
-    MTreeNode* startNode = makeLadderTree(MTreeNode::beginTree(0, 0), mazeRows);
-
-    fillMaze(maze2, startNode);
-
-    maze2.printMaze();
-
-    printTraversalField(startNode, mazeRows, mazeColumns);
+    maze.printMaze();
 }
