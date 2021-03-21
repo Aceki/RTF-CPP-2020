d8b3063ce1d224b6f144df376b205f24bc2ece5e
Aceki <aceki320@gmail.com>
2021-03-21 19:35:43 +0500

    Rename variables

--- Task18032021/tests/shared_pointer_tests.cpp
+++ Task18032021/tests/shared_pointer_tests.cpp
@@ -8,26 +8,26 @@ using Pointers::SharedPointer;
 
 TEST(SharedPointerTests, CastingEmptyPointerToBool_Should_False) 
 {
-    SharedPointer<ExampleClass> sp;
+    SharedPointer<ExampleClass> sptr;
 
-    ASSERT_FALSE((bool)sp);
+    ASSERT_FALSE((bool)sptr);
 }
 
 TEST(SharedPointerTests, CastingNonEmptyPointerToBool_Should_True) 
 {
-    SharedPointer<ExampleClass> sp(new ExampleClass(1));
+    SharedPointer<ExampleClass> sptr(new ExampleClass(1));
 
-    ASSERT_TRUE((bool)sp);
+    ASSERT_TRUE((bool)sptr);
 }
 
 TEST(SharedPointerTests, UseCount_Should_WorkCorrect) 
 {
-    SharedPointer<ExampleClass> sp1(new ExampleClass(1));
-    SharedPointer<ExampleClass> sp2(sp1);
-    SharedPointer<ExampleClass> sp3(new ExampleClass(3));
+    SharedPointer<ExampleClass> sptr1(new ExampleClass(1));
+    SharedPointer<ExampleClass> sptr2(sptr1);
+    SharedPointer<ExampleClass> sptr3(new ExampleClass(3));
 
-    ASSERT_EQ(sp1.useCount(), 2);
-    ASSERT_EQ(sp3.useCount(), 1);
+    ASSERT_EQ(sptr1.useCount(), 2);
+    ASSERT_EQ(sptr3.useCount(), 1);
 }
 
 TEST(SharedPointerTests, Swap_Should_Work_Correct) 
@@ -35,34 +35,34 @@ TEST(SharedPointerTests, Swap_Should_Work_Correct)
     ExampleClass* obj1 = new ExampleClass(1);
     ExampleClass* obj2 = new ExampleClass(2);
 
-    SharedPointer<ExampleClass> sp1(obj1);
-    SharedPointer<ExampleClass> sp2(sp1);
-    SharedPointer<ExampleClass> sp3(obj2);
+    SharedPointer<ExampleClass> sptr1(obj1);
+    SharedPointer<ExampleClass> sptr2(sptr1);
+    SharedPointer<ExampleClass> sptr3(obj2);
 
-    sp1.swap(sp3);
+    sptr1.swap(sptr3);
 
-    ASSERT_EQ(sp1.get(), obj2);
-    ASSERT_EQ(sp1.useCount(), 1);
+    ASSERT_EQ(sptr1.get(), obj2);
+    ASSERT_EQ(sptr1.useCount(), 1);
 
-    ASSERT_EQ(sp2.get(), obj1);
-    ASSERT_EQ(sp2.useCount(), 2);
+    ASSERT_EQ(sptr2.get(), obj1);
+    ASSERT_EQ(sptr2.useCount(), 2);
 
-    ASSERT_EQ(sp3.get(), obj1);
-    ASSERT_EQ(sp3.useCount(), 2);
+    ASSERT_EQ(sptr3.get(), obj1);
+    ASSERT_EQ(sptr3.useCount(), 2);
 }
 
 TEST(SharedPointerTests, ComparisonOperator_Should_True_When_Copy) 
 {
-    SharedPointer<ExampleClass> sp1(new ExampleClass(1));
-    SharedPointer<ExampleClass> sp2(sp1);
+    SharedPointer<ExampleClass> sptr1(new ExampleClass(1));
+    SharedPointer<ExampleClass> sptr2(sptr1);
 
-    ASSERT_EQ(sp1, sp2); // ASSERT_EQ uses operator ==
+    ASSERT_EQ(sptr1, sptr2); // ASSERT_EQ uses operator ==
 }
 
 TEST(SharedPointerTests, CopyConstructor_Should_Increment_Count) 
 {
-    SharedPointer<ExampleClass> sp1(new ExampleClass(1));
-    SharedPointer<ExampleClass> sp2 = sp1;
+    SharedPointer<ExampleClass> sptr1(new ExampleClass(1));
+    SharedPointer<ExampleClass> sptr2 = sptr1;
 
-    ASSERT_EQ(sp1.useCount(), 2); // ASSERT_EQ uses operator ==
+    ASSERT_EQ(sptr1.useCount(), 2);
 }
