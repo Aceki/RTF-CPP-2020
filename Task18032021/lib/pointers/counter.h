d1b1e43a04e01e7b79f2f76299552e022f1a71fa
Aceki <aceki320@gmail.com>
2021-03-21 19:25:49 +0500

    Add 'Pointers' namespace to 'counter.h'

--- Task18032021/lib/pointers/counter.h
+++ Task18032021/lib/pointers/counter.h
@@ -1,19 +1,22 @@
 #pragma once
 
-template<class T>
-class Counter
+namespace Pointers 
 {
-public:
-    Counter(T* ptr) : m_pointer(ptr) { }
+    template<class T>
+    class Counter
+    {
+    public:
+        Counter(T* ptr) : m_pointer(ptr) { }
 
-    T* pointer() const { return m_pointer; }
+        T* pointer() const { return m_pointer; }
 
-    int count() const { return m_count; }
+        int count() const { return m_count; }
 
-    int increase() { return ++m_count; }
-    
-    int decrease() { return --m_count; }
-private:
-    int m_count = 1;
-    T* m_pointer = nullptr;
-};
+        int increase() { return ++m_count; }
+        
+        int decrease() { return --m_count; }
+    private:
+        int m_count = 1;
+        T* m_pointer = nullptr;
+    };
+}
