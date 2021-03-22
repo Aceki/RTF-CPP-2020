dc423ae06ec61e6aaebea71395ad47866630f159
Aceki <aceki320@gmail.com>
2021-03-22 10:16:08 +0500

    Add 'Counter' destructor. Delete copy constructor. Delete assigment operator

--- Task18032021/lib/pointers/counter.h
+++ Task18032021/lib/pointers/counter.h
@@ -8,6 +8,8 @@ namespace Pointers
     public:
         Counter(T* ptr) : m_pointer(ptr) { }
 
+        Counter(const Counter<T>&) = delete;
+
         T* pointer() const { return m_pointer; }
 
         int count() const { return m_count; }
@@ -15,6 +17,10 @@ namespace Pointers
         int increase() { return ++m_count; }
         
         int decrease() { return --m_count; }
+
+        Counter<T>& operator=(const Counter<T>&) = delete;
+
+        ~Counter() { delete m_pointer; }
     private:
         int m_count = 1;
         T* m_pointer = nullptr;
